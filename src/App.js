e666470c5141e81fe3e29c3011b224d5b4b6abc2
HarisHassan55 <harishassan551@gmail.com>
2024-07-24 03:15:59 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Open this in a new window!</h1>
+      <h2>
+        That way it'll open your camera so you can scan the QR code from your
+        phone!
+      </h2>
+      <p>Or make sure this page has camera access</p>
+    </div>
+  );
+}
